f3576e96a0220d7b2bd0d5055e4b216ad9e63694
hyeokjin-eun <gurwls2399@gmail.com>
2023-01-03 23:57:22 +0900

    feat : decorator study

---
+++ src/main/java/com/pattern/design/decorator/example/Client.java
@@ -0,0 +1,14 @@
+package com.pattern.design.decorator.example;
+
+public class Client {
+
+    private final CommentService commentService;
+
+    public Client(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    public void writeComment(String comment) {
+        commentService.addComment(comment);
+    }
+}
---
+++ src/main/java/com/pattern/design/decorator/example/TrimmingCommentDecorator.java
@@ -0,0 +1,17 @@
+package com.pattern.design.decorator.example;
+
+public class TrimmingCommentDecorator extends CommentDecorator {
+
+    public TrimmingCommentDecorator(CommentService commentService) {
+        super(commentService);
+    }
+
+    @Override
+    public void addComment(String comment) {
+        super.addComment(trim(comment));
+    }
+
+    private String trim(String comment) {
+        return comment.replace("...", "");
+    }
+}
